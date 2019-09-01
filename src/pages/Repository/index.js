19e7d5c31b2268cd1e948dae8ec58d95d42b7fc8
Guigso <gui.soliveira@hotmail.com>
2019-09-01 20:16:45 -0300

    aula done

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
-// import { Container } from './styles';
+import { Loading, Owner, IssueList } from './styles';
+
+import Container from '../../components/Container/index';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -22,25 +25,66 @@ export default class Repository extends Component {
     const { match } = this.props;
 
     const repoName = decodeURIComponent(match.params.repository);
+    if (!localStorage.getItem('repo')) {
+      const [repository, issues] = await Promise.all([
+        await api.get(`/repos/${repoName}`),
+        await api.get(`/repos/${repoName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
 
-    // const [repository, issues] = await Promise.all([
-    //   await api.get(`/repos/${repoName}`),
-    //   await api.get(`/repos/${repoName}/issues`, {
-    //     params: {
-    //       state: 'open',
-    //       per_page: 5,
-    //     },
-    //   }),
-    // ]);
-
-    this.setState({
-      repository: 'repository.data',
-      issues: 'issues.data',
-      loading: false,
-    });
+      localStorage.setItem('repo', JSON.stringify(repository.data));
+      localStorage.setItem('issues', JSON.stringify(issues.data));
+      this.setState({
+        repository: repository.data,
+        issues: issues.data,
+        loading: false,
+      });
+    } else {
+      const repo = JSON.parse(localStorage.getItem('repo'));
+      const issues = JSON.parse(localStorage.getItem('issues'));
+      this.setState({
+        repository: repo,
+        issues,
+        loading: false,
+      });
+    }
   }
 
   render() {
-    return <h1>Repository</h1>;
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
   }
 }
